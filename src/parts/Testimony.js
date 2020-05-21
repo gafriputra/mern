fcb5dfe345a77c9c7cceba896ad1b38905aa2e7f
gafriputra <gafri.putra@gmail.com>
2020-05-21 11:35:24 +0700

    Create Star element, and slicing color variable

---
+++ src/parts/Testimony.js
@@ -0,0 +1,11 @@
+import React from "react";
+// import Fade from "react-reveal/Fade";
+
+// import TestimonyAccent from "assets/images/testimonial-landingpages-frame.jpg";
+
+import Star from "elements/Star";
+// import Button from "elements/Button";
+
+export default function Testimony({ data }) {
+  return <Star value={2} width={35} height={35} spacing={4}></Star>;
+}
